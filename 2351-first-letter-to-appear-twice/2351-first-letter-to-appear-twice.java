03ba1796da27080089b9cde6bb82419752af8a1f
AXAT007 <axatthakur2006@gmail.com>
2026-07-08 11:46:23 +0530

    Time: 0 ms (100%), Space: 42.9 MB (51.87%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -1,25 +1,15 @@
 class Solution {
     public char repeatedCharacter(String s) {
-         // HashMap <Character,Integer> map=new HashMap<>();
-        // for(int i=0;i<s.length();i++){
-        //     char c=s.charAt(i);
-        //     map.put(c,map.getOrDefault(c,0)+1);
-        // }
-        // for(int i=0;i<s.length();i++){
-        //     char c=s.charAt(i);
-        //     if(map.get(c)==1){
-        //         return i;
-        //     }
-        // }
+       
         int [] arr=new int[26];
         for(int i=0;i<s.length();i++){
             char c=s.charAt(i);
-            arr[c-'a']++;
-            if(arr[c-'a']>1){
+            
+            if(arr[c-'a']==1){
                 return c;
             }
-            
+            arr[c-'a']++;
         }
-        return 'l';
+        return ' ';
     }
 }
