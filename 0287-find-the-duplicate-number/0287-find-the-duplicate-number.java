b6ad679992f8d57ee166931594172a05510aee3e
AXAT007 <axatthakur2006@gmail.com>
2026-07-09 12:02:56 +0530

    Time: 4 ms (90.74%), Space: 83 MB (46.51%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,12 +1,56 @@
+/*class Solution {
+    public int findDuplicate(int[] nums) {
+        //  if(nums.length==2) return nums[0];
+        int slow=0;
+        int fast=0;
+        // while( slow==fast){
+        //     if(slow==fast){
+        //         return nums[fast];
+        //     }
+        //     slow=nums[slow+1];
+        //     fast=nums[fast+2];
+        
+        //     if(slow==nums.length-1){
+        //         slow=0;
+        //     }
+        //     if(fast==nums.length-1){
+        //         fast=1;
+        //     }
+        //     if(fast==nums.length-2){
+        //         fast=0;
+        //     }
+        //  }
+
+
+        // full on cheating coulfnt understd it
+        do {
+    slow = nums[slow];          // one step
+    fast = nums[nums[fast]];    // two steps
+} while (slow != fast);
+        return nums[fast];
+    }
+}*/
+
+
+
+// ? cheat cheat cheat thst is all u know
 class Solution {
     public int findDuplicate(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int val:nums){
-            map.put(val,map.getOrDefault(val,0)+1);
-            if(map.get(val)==2){
-                return val;
-            }
+        int slow = 0;
+        int fast = 0;
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = 0;
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return -1;
+
+        return slow;
     }
 }
