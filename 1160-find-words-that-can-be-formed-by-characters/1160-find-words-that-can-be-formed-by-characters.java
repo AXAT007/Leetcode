ba751ae0e51fabbe2aa26002130f205747956a32
AXAT007 <axatthakur2006@gmail.com>
2026-07-25 23:30:51 +0530

    Time: 6 ms (86.54%), Space: 47.1 MB (95.67%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
@@ -5,36 +5,31 @@ class Solution {
             char c=chars.charAt(i);
             chaar[c-'a']++;
         }
-        int [][] word=new int[words.length][26];
-        for(int j=0;j<words.length;j++){
-            String magazine=words[j];
-
-            for(int i=0;i<magazine.length();i++){
-            char c=magazine.charAt(i);
-            word[j][c-'a']++;
-             }
-        }
+       
+            boolean valid=true;
 
         int ans=0;
         for(int j=0;j<words.length;j++){
-            int [] temp=word[j];
-            int count=0;
-            boolean valid=true;
+            String magazine=words[j];
+            int [] word=new int [26];
+            for(int i=0;i<magazine.length();i++){
+            char c=magazine.charAt(i);
+            word[c-'a']++;
+             }
+             
+            valid=true;
             for(int i=0;i<26;i++){
-                if(temp[i]==0&&chaar[i]!=0){}
-                else if(temp[i]>chaar[i]){
+                 if(word[i]>chaar[i]){
                     valid=false;
                     break;
-                }else{
-                    count+=temp[i];
-                }
+                } 
             }
             
             if(valid){
-                ans+=count;
+                ans+=magazine.length();
             }
-            valid=true;
-            count=0;
+            
+         
         }
         return ans;
     }   
