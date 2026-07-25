89f677818938bbfbcec5446b7fde240595ac97eb
AXAT007 <axatthakur2006@gmail.com>
2026-07-25 10:02:12 +0530

    Time: 1 ms (100%), Space: 42.6 MB (74.48%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -1,39 +1,22 @@
 class Solution {
     public int maxProduct(int n) {
-        int ans=1;
         int first=0;
         int sec=0;
-        // while(n>0){
-        //     if(first==0){
 
-        //     }
-        // }
-
-        int size=0;
-        int temp=n;
-        for(;temp>0;temp=temp/10){
-            size++;
-            
-        }
-        int []  arr=new int [size];
-        temp=n;
-        for(int i=0;i<size;i++){
-            arr[i]=temp%10;
-            temp/=10;
-        }
-
-        for(int val:arr){
-            first=Math.max(first,val);
-        }
-        for(int i=0;i<size;i++){
-            if(arr[i]==first){
-                arr[i]=-1;
-                break;
+         int val=0;
+        while(n>0){
+            val=n%10;
+            if(val>first){
+                int t=first;
+                first=val;
+                sec=Math.max(sec,t);
             }
+            else{
+                sec=Math.max(sec,val);
+            }
+            n/=10;
         }
-        for(int val:arr){
-            sec=Math.max(sec,val);
-        }
+       
         return first*sec;
     }
 }
