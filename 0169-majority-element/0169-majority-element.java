78caf6566f28c2ebe88b1516fb993308aa40bacb
AXAT007 <axatthakur2006@gmail.com>
2026-07-27 19:24:04 +0530

    Time: 5 ms (52.01%), Space: 55.8 MB (32.07%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,18 +3,19 @@ class Solution {
         int ans=0;
         Arrays.sort(nums);
         int n=nums.length/2;
-        int count=1;
-        for(int i=1;i<nums.length;i++){
-              if(nums[i]==nums[i-1]){
-                    count++;
-                     if(count> n) {
-                return nums[i];
-            }
-                }
-                else{
-                    count=1;
-                }
-            }
-         return nums[nums.length-1];
+        return nums[n];
+        // int count=1;
+        // for(int i=1;i<nums.length;i++){
+        //       if(nums[i]==nums[i-1]){
+        //             count++;
+        //              if(count> n) {
+        //         return nums[i];
+        //     }
+        //         }
+        //         else{
+        //             count=1;
+        //         }
+        //     }
+        //  return nums[nums.length-1];
     }
 }
