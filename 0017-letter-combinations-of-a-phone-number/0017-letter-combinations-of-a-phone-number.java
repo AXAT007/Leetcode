e81086b0982bd85934cdc9c7a593cdf183484631
Axat Thakur <axatthakur2006@gmail.com>
2026-08-06 00:31:03 +0530

    Time: 0 ms (100%), Space: 43.5 MB (64.26%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,45 +1,38 @@
 class Solution {
+    String[] map = {
+        "",     
+        "",     
+        "abc", 
+        "def",  
+        "ghi",  
+        "jkl",  
+        "mno",  
+        "pqrs",
+        "tuv",  
+        "wxyz" 
+    };
     public List<String> letterCombinations(String digits) {
         if (digits.length() == 0) {
     return new ArrayList<>();
 }
-        HashMap<Integer,String> map=new HashMap<>();
-        int count=0;
-        for(int i=2;i<=9;i++){
-            String s="";
-            if(i!=7&&i!=9){
-                for(int j=0;j<3;j++){
-                    s+=(char) (count+'a') ;
-                    count++;
-                }
-            }
-            else{
-                for(int j=0;j<4;j++){
-                    s+=(char) (count+'a') ;
-                    count++;
-                }
-            }
-            map.put(i,s);
-        }
+    
         StringBuilder sb=new StringBuilder();
         List<String> ans=new ArrayList<>();
-        solve(digits,ans,sb,0,map);
+        solve(digits,ans,sb,0);
         return ans;
     }
 
-    void solve(String digits,List<String> ans,StringBuilder sb,int j,HashMap<Integer,String> map){
+    void solve(String digits,List<String> ans,StringBuilder sb,int j){
         if(j==digits.length()){
-            ans.add(new String(sb.toString()));
+            ans.add( sb.toString());
             return;
         }
 
-        // map.get( digits.charAt(j) -'0').length()
-
-        String letters = map.get(digits.charAt(j) - '0');
+        String letters = map[digits.charAt(j) - '0'];
         for(int i=0;i< letters.length();i++){
             
             sb.append(letters.charAt(i));
-            solve(digits,ans,sb,j+1,map);
+            solve(digits,ans,sb,j+1);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
