11302452801eae1a6e65de832bc82ebfacfc401a
Axat Thakur <axatthakur2006@gmail.com>
2026-08-09 22:11:17 +0530

    Time: 24 ms (49.3%), Space: 48.2 MB (38.47%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
@@ -1,35 +1,48 @@
 class Solution {
-    public boolean checkValid(int[][] matrix) {
-        int n=matrix.length;
-        // int sum=n*(n+1)/2;
-       
-        int sum1=0;
-        int sum2=0;
-        for(int i=1;i<=n;i++){
-            sum1^=i;
-            sum2^=i;
-        }
+     public boolean checkValid(int[][] matrix) {
         for(int i=0;i<matrix.length;i++){
-            // int curr1=0;
-            // int curr2=0;
-             HashSet<Integer> set1=new HashSet<>();
-        HashSet<Integer> set2=new HashSet<>();
-            int curr1=sum1;
-            int curr2=sum2;
+            HashSet<Integer> set1=new HashSet<>();
+            HashSet<Integer> set2=new HashSet<>();
             for(int j=0;j<matrix[i].length;j++){
                 if(!set1.add(matrix[i][j])||!set2.add(matrix[j][i])){
                     return false;
                 }
-                curr1^=matrix[i][j];
-                curr2^=matrix[j][i];
-            }
-            // if(curr1!=sum||curr2!=sum){
-            //     return false;
-            // }
-            if(curr1!=0||curr2!=0){
-                return false;
             }
         }
         return true;
     }
+
+    // public boolean checkValid(int[][] matrix) {
+    //     int n=matrix.length;
+    //     // int sum=n*(n+1)/2;
+       
+    //     int sum1=0;
+    //     int sum2=0;
+    //     for(int i=1;i<=n;i++){
+    //         sum1^=i;
+    //         sum2^=i;
+    //     }
+    //     for(int i=0;i<matrix.length;i++){
+    //         // int curr1=0;
+    //         // int curr2=0;
+    //          HashSet<Integer> set1=new HashSet<>();
+    //     HashSet<Integer> set2=new HashSet<>();
+    //         int curr1=sum1;
+    //         int curr2=sum2;
+    //         for(int j=0;j<matrix[i].length;j++){
+    //             if(!set1.add(matrix[i][j])||!set2.add(matrix[j][i])){
+    //                 return false;
+    //             }
+    //             curr1^=matrix[i][j];
+    //             curr2^=matrix[j][i];
+    //         }
+    //         // if(curr1!=sum||curr2!=sum){
+    //         //     return false;
+    //         // }
+    //         if(curr1!=0||curr2!=0){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
 }
