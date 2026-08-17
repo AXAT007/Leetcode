15495324359aa1dc33b3dd2a6528f8a04b0cf4c3
Axat Thakur <axatthakur2006@gmail.com>
2026-08-17 19:48:46 +0530

    Time: 7 ms (5.33%), Space: 49.3 MB (13.7%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -7,7 +7,7 @@ class Solution {
         }
         for(int i=0;i< isConnected.length;i++){
             for(int j=0;j<isConnected[i].length;j++){
-                if(isConnected[i][j]==1){
+                if(isConnected[i][j]==1&& i!=j){
                     list.get(i).add(j);
                     list.get(j).add(i);
                 }
