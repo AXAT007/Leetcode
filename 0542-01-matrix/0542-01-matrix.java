6f92b7a3e61a90c4ee14fdf2d7dd7ae51282fffb
Axat Thakur <axatthakur2006@gmail.com>
2026-08-19 21:47:34 +0530

    Time: 15 ms (42.15%), Space: 49.1 MB (51.55%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -7,71 +7,66 @@ class Pair{
     }
 }
     
-    class Solution {
-    public int[][] updateMatrix(int[][] mat) {
-        // int [][] ans=new int[mat.length][];
-        // for(int i=0;i<mat.length;i++){
-        //     ans[i]=new int[mat[i].length];
-        // }
-        ArrayDeque<Pair> q=new ArrayDeque<>();
-        int one =0;
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[i].length;j++){
-                int val=mat[i][j];
-                if(val==0){
-                    // ans[i][j]=0;
+class Solution {
+    public int[][] updateMatrix(int[][] grid) {
+        int one=0;
+        int dist=1;
+        Queue<Pair> q=new LinkedList<>();
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+                if(grid[i][j]==0){
+                    
+                // grid[i][j]=-1;
                     q.offer(new Pair(i,j));
                 }
                 else{
-                    mat[i][j]=-1;
+                    grid[i][j]=-1;
                     one++;
                 }
             }
         }
-        int count=1;
         while(one>0&& !q.isEmpty()){
-            
             int size=q.size();
-            while(one>0&&size>0){
+            while(size>0&& one>0){
                 int i=q.peek().a;
                 int j=q.peek().b;
                 q.poll();
-               if(isValid(mat,i+1,j)){
-                    // ans[i+1][j]=count;
-                    one--;
-                    mat[i+1][j]=count;
+                if(isVlaid(i+1,j,grid)){
+                    grid[i+1][j]=dist;
                     q.offer(new Pair(i+1,j));
-                }
-                  if(isValid(mat,i-1,j)){
                     one--;
-                    
-                    mat[i-1][j]=count;
-                    // ans[i-1][j]=count;
+                }
+                if(isVlaid(i-1,j,grid)){
+                    grid[i-1][j]=dist;
                     q.offer(new Pair(i-1,j));
-                }
-                  if(isValid(mat,i,j+1)){
                     one--;
-                    
-                    mat[i][j+1]=count;
-                    // ans[i][j+1]=count;
+                }
+                if(isVlaid(i,j+1,grid)){
+                    grid[i][j+1]=dist;
                     q.offer(new Pair(i,j+1));
-                }
-                  if(isValid(mat,i,j-1)){
                     one--;
-                    mat[i][j-1]=count;
-                    // ans[i][j-1]=count;
+                }
+                if(isVlaid(i,j-1,grid)){
+                    grid[i][j-1]=dist;
                     q.offer(new Pair(i,j-1));
+                    one--;
                 }
                 size--;
             }
-            count++;
+            dist++;
         }
-        return mat;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+                if(grid[i][j]==-1){
+                    grid[i][j]=0;
+                }
+            }
+        }
+        
+        return grid;
+        
     }
-    boolean isValid(int [][] mat,int i,int j){
-        if(i>=0&&j>=0&&i<mat.length&&j<mat[i].length&&mat[i][j]==-1){
-            return true ;
-        }
-        return false;
+    boolean isVlaid(int i,int j,int [][] grid){
+        return i>=0&&j>=0&&i<grid.length&&j<grid[i].length&&grid[i][j]== -1;
     }
 }
