77fc0761318937cd480023d05a9f1848cf93f830
Axat Thakur <axatthakur2006@gmail.com>
2026-08-28 23:53:35 +0530

    Time: 2 ms (97.72%), Space: 47.1 MB (42.4%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,73 +1,48 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        for(int i=0;i<asteroids.length;i++){
-            if(asteroids[i]<0){
-                left(asteroids,i);
+        Deque<Integer> st=new ArrayDeque<>();
+        int i=0;
+        while(i<asteroids.length){
+            int x=asteroids[i];
+            if(st.isEmpty()) {
+                st.push(x);
+                i++;
             }
-            else if(asteroids[i]>0){
-                right(asteroids,i);
+            else{
+                int y=st.peek();
+                
+                // both +
+                
+                if((y>0&&x>0 )|| y<0 || (y<0&&x<0)){
+                    st.push(x);
+                    i++;
+                }
+
+
+                // +,-
+                else {
+                // + == -
+                    if(x==y*-1){
+                        st.pop();
+                        i++;
+                    }
+
+                    // st has + a
+                    else if(x*-1>y){
+                        st.pop();
+                        // st.push(x);
+                    } 
+                    else{
+                        i++;
+                    }
+                }
             }
         }
-        int count=0;
-        for(int i=0;i<asteroids.length;i++){
-            if(asteroids[i]!=0){
-                count++;
-            }
-        }
-        int j=0;
-        int [] ans=new int[count];
-        for(int i=0;i<asteroids.length;i++){
-            if(asteroids[i]!=0){
-                ans[j]=asteroids[i];
-                j++;
-            }
+          i=st.size()-1;
+        int [] ans=new int[st.size()];
+        while(!st.isEmpty()){
+            ans[i--]=st.pop();
         }
         return ans;
-    }
-    void left(int [] arr,int i){
-        int b=Math.abs(arr[i]);
-        for(int j=i-1;j>=0;j--){
-            int a=arr[j];
-            if(a<0){
-                return;
-            }
-            else if(a>0){
-                if(a>b){
-                    arr[i]=0;
-                    return;
-                }
-                else if(a<b){
-                    arr[j]=0;
-                }
-                else if(a==b){
-                    arr[i]=0;
-                    arr[j]=0;
-                    return;
-                }
-            }
-        }
-    }
-      void right(int [] arr,int i){
-        
-        for(int j=i+1;j<arr.length;j++){
-            if(arr[j]>0){
-                return;
-            }
-            else if(arr[j]<0){
-                if(Math.abs(arr[j])>arr[i]){
-                    arr[i]=0;
-                    return;
-                }
-                else if(Math.abs(arr[j])<arr[i]){
-                    arr[j]=0;
-                }
-                
-                else{
-                   arr[i]=0;
-                    arr[j]=0;
-                   return;
-                }
-            }
-        }
-    }
+    } 
 }
