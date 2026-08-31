f3637555c81c067a1b8475b86d381e4600b160e7
Axat Thakur <axatthakur2006@gmail.com>
2026-08-31 21:10:38 +0530

    Time: 6 ms (85.51%), Space: 45.9 MB (98.98%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -0,0 +1,57 @@
+class Pair{
+    int a;
+    int wt;
+    Pair(int a,int b){
+        this.a=a;
+        wt=b;
+    }
+}
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        
+        ArrayList<ArrayList<Pair>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        int [] cost=new int[n];
+        Arrays.fill(cost,Integer.MAX_VALUE);
+        cost[src]=0;
+        for(int[] i:flights){
+            int node=i[0];
+            int neigb=i[1];
+            int wt=i[2];
+            adj.get(node).add(new Pair(neigb,wt));
+        }
+Queue<Pair> pq=new ArrayDeque<>();
+        // PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.wt - b.wt);
+        pq.offer(new Pair(src,0));
+int ans = Integer.MAX_VALUE;
+
+while (k > -1 && !pq.isEmpty()) {
+    int size=pq.size();
+    while(size>0){
+    int node = pq.peek().a;
+    int wt = pq.peek().wt;
+    pq.poll();
+
+    for (Pair p : adj.get(node)) {
+        int total_wt = wt + p.wt;
+
+        if (p.a == dst) {
+            ans = Math.min(ans, total_wt);
+        } else {
+            if(cost[p.a]>total_wt){
+                cost[p.a]=total_wt;
+                pq.offer(new Pair(p.a, total_wt));
+
+            }
+        }
+    }
+    size--;
+    }
+    k--;
+}
+if(ans==Integer.MAX_VALUE) return -1;
+return ans;
+    }
+}
