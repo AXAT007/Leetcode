e4fc4d3516f60bf57f60958329c0f3cf2a7a8126
Axat Thakur <axatthakur2006@gmail.com>
2026-09-06 17:27:53 +0530

    Time: 35 ms (94.08%), Space: 286.2 MB (35.61%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,17 +1,63 @@
 class Solution {
+
+// dsu
+
+    int getParent(int [] parent,int u){
+        if(parent[u]==u) return u;
+        return parent[u]=getParent(parent,parent[u]);
+    }
+
+    void unionByRank(int [] parent,int [] rank,int u,int v){
+        int pu=getParent(parent,u);
+        int pv=getParent(parent,v);
+        if(pu==pv) return ;
+        if(rank[pu]==rank[pv]){
+            rank[pu]++;
+            parent[pv]=pu;
+        }
+        else if(rank[pu]<rank[pv]){
+            parent[pu]=pv;
+        }
+        else{
+            parent[pv]=pu;
+        }
+    }
+ 
     public boolean validPath(int n, int[][] edges, int source, int destination) {
+
+        if(source==destination )return true;
         ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
         for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
+        // for(int []e :edges){
+        //     adj.get(e[0]).add(e[1]);
+        //     adj.get(e[1]).add(e[0]);
+        // }
+        // boolean [] visited=new boolean[n];
+        // return dfs(adj,visited,source,destination);
+        // return bfs(adj,visited,source,destination);
+        return DSU_unionFind(adj,edges,n,source,destination);
+    }
+
+    boolean DSU_unionFind(ArrayList<ArrayList<Integer>> adj ,int [][] edges,int n,int src, int dest){
         for(int []e :edges){
             adj.get(e[0]).add(e[1]);
-            adj.get(e[1]).add(e[0]);
         }
-        boolean [] visited=new boolean[n];
-        // return dfs(adj,visited,source,destination);
-        return bfs(adj,visited,source,destination);
-
+        int[] rank=new int[n];
+        int[] parent=new int[n];
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+        for(int u=0;u<n;u++){
+            for(int v:adj.get(u)){
+                unionByRank(parent,rank,u,v);
+                if(getParent(parent,src)==getParent(parent,dest)){
+                    return true;
+                }
+            }
+        }
+        return false;
         
     }
 
