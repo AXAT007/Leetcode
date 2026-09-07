d62feba7c35958a1f240c49cd6201afcb039929b
Axat Thakur <axatthakur2006@gmail.com>
2026-09-07 20:58:59 +0530

    Time: 3 ms (99.67%), Space: 65.9 MB (81.26%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,49 +1,58 @@
-// class Solution {
-// int[] rank;
-// int[] parent;
-//     int getParent(int [] parent,int u){
-//         if(parent[u]==u) return u;
-//         return parent[u]=getParent(parent,parent[u]);
-//     }
 
-//     boolean unionByRank(int [] parent,int [] rank,int u,int v){
-//         int pu=getParent(parent,u);
-//         int pv=getParent(parent,v);
-//         if(pu==pv) return true;
-//         if(rank[pu]==rank[pv]){
-//             rank[pu]++;
-//             parent[pv]=pu;
-//         }
-//         else if(rank[pu]<rank[pv]){
-//             parent[pu]=pv;
-//         }
-//         else{
-//             parent[pv]=pu;
-//         }
-//         return false;
-//     }
-//     public int makeConnected(int n, int[][] connections) {
-//         if(connections.length<n-1) return -1;
+// /*
 
-//         int[] rank=new int[n];
-//         int[] parent=new int[n];
-//         for(int i=0;i<n;i++){
-//             parent[i]=i;
-//         }
-//         int components=n;
-//         int cable=connections.length;
-//         for(int []e:connections){
-//             int u=e[0];
-//             int v=e[1];
-//             if(!unionByRank(parent,rank,u,v)){
-//                 // cable--;
-//                 components--;
-//             }
+class Solution {
+
+    int getParent(int [] parent,int u){
+        if(parent[u]==u) return u;
+        return parent[u]=getParent(parent,parent[u]);
+    }
+
+    boolean unionByRank(int [] parent,int [] rank,int u,int v){
+        int pu=getParent(parent,u);
+        int pv=getParent(parent,v);
+        if(pu==pv) return true;
+        if(rank[pu]==rank[pv]){
+            rank[pu]++;
+            parent[pv]=pu;
+        }
+        else if(rank[pu]<rank[pv]){
+            parent[pu]=pv;
+        }
+        else{
+            parent[pv]=pu;
+        }
+        return false;
+    }
+    public int makeConnected(int n, int[][] connections) {
+        
+        boolean [] visited=new boolean[n];
+        int[] rank=new int[n];
+        int[] parent=new int[n];
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+        int cable=connections.length;
+        for(int []e:connections){
+            int u=e[0];
+            int v=e[1];
+            if(!unionByRank(parent,rank,u,v)){
+                cable--;
+            }
                 
-//         }
-//         return components-1;;
-//     }
-// }
+        }
+        int root=0;
+        for(int i=0;i<n;i++){
+            if(parent[i]==i){
+                root++;
+            }
+        }
+        return cable<root-1?-1:root-1;
+    }
+}
+/*
+
+// dont underratand this one properly the main thing is connected 
 
 class Solution {
 
@@ -99,3 +108,4 @@ class Solution {
         return components - 1;
     }
 }
+*/
