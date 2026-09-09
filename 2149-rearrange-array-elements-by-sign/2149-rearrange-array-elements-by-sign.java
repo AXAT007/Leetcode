cceccbf0a2a39ad2d076c84aa309654a9e34838d
Axat Thakur <axatthakur2006@gmail.com>
2026-09-09 21:09:51 +0530

    Time: 3 ms (100%), Space: 175.2 MB (75.51%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,52 +1,22 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        // ture -> +ve 
-        // false -> -ve
-        
-        // boolean flag = false;
-        // if(nums[0]>=0){
-        //     flag=true;
-        // }
-        // int j=0;
-        // for(int i=1;i<nums.length;i++){
-        //     if(flag){
-        //         if(nums[i]<0){
-        //             j++;
-        //             flag=false;
-        //             swap(nums,i,j);
-        //         }
-        //     }
-        //     else{
-        //         if(nums[i]>=0){
-        //             j++;
-        //             flag=true;
-        //             swap(nums,i,j);
-        //         }
-        //     }
-        // }
-        // return nums;
+  
 
         int n=nums.length;
         int [] ans=new int[n];
-        int i=0;
+        int pos=0;
+        int neg=1;
         for(int val:nums){
             if(val>=0){
-                ans[i]=val;
-                i=i+2;
+                ans[pos]=val;
+                pos=pos+2;
             }
-        }
-        i=1;
-        for(int val:nums){
-            if(val<0){
-                ans[i]=val;
-                i=i+2;
+            else{
+                ans[neg]=val;
+                neg=neg+2;
             }
         }
         return ans;
     }
-    void swap(int [] nums,int i,int j){
-        int t=nums[i];
-        nums[i]=nums[j];
-        nums[j]=t;
-    }
+ 
 }
