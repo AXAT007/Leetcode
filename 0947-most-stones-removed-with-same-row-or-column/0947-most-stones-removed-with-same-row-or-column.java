bc899ab9c3d8a06f9999703d0741a4e9c50b21a5
Axat Thakur <axatthakur2006@gmail.com>
2026-09-11 23:52:26 +0530

    Time: 49 ms (10.6%), Space: 46.5 MB (91.77%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,78 @@
+class Solution {
+    int[] parent;
+    int []rank;
+
+    int findParent(int u){
+        if(parent[u]==u) return u ;
+        return parent[u]=findParent(parent[u]);
+    }
+
+    boolean union(int u,int v){
+        int pu=findParent(u);
+        int pv=findParent(v);
+        if(pu==pv) return true;
+        if(rank[pu]==rank[pv]){
+            parent[pv]=pu;
+            rank[pu]++;
+        }
+        
+        else if(rank[pu]>rank[pv]){
+            parent[pv]=pu;
+        }
+        else{
+            parent[pu]=pv;
+        }
+        return false;
+    }
+    
+    public int removeStones(int[][] stones) {
+        int n=0;
+        int m=0;
+        for(int []e:stones){
+            int u=e[0];
+            int v=e[1];
+            n=Math.max(n,u);
+            m=Math.max(m,v);
+        }
+        n++;
+        m++;
+        parent=new int[n+m];
+        rank=new int[n+m];
+        Arrays.fill(parent,-1);
+        int ans=0;
+        int count=0;
+        for(int []e :stones){
+            
+            int u=e[0];
+            int v=e[1]+n;
+            if(parent[u]==-1){
+                parent[u]=u;
+            }
+            if(parent[v]==-1){
+                parent[v]=v;
+            }
+            if(union(u,v)){
+                ans++;
+                System.out.println(" could not connect ");
+            }
+            else{
+                count++;
+                System.out.println(" connect ");
+            }
+        }
+        
+        System.out.println(" ans : "+ ans);
+        System.out.println(" count : "+ count);
+        count=0;
+        ans=0;
+        for(int i=0;i<parent.length;i++){
+            if(parent[i]==-1) continue;
+            else if(parent[i]==i) count++;
+            else ans++;
+        }
+        System.out.println(" ans : "+ ans);
+        System.out.println(" count : "+ count);
+        
+        return stones.length-count;
+    }
+}
